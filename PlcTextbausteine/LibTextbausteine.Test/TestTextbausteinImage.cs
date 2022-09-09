3c7e7e2ec2c5233934122737e579e4da1be24df5
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2022-09-09 18:54:24 +0200

    Bilder testen - noch nicht fertig

---
+++ PlcTextbausteine/LibTextbausteine.Test/TestTextbausteinImage.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace LibTextbausteine.Test;
+
+public class TestTextbausteineImage
+{
+    [Theory]
+    [InlineData(4517, "zip13/json.zip", "json2//inhalt.json", 2)]
+
+    public void TestsInhalt(int laenge, string jsonZip, string pfadJson, int id)
+    {
+        var alleTextbausteine = new Textbausteine(jsonZip);
+        var listeTextbausteine = new ListeTextbausteine(pfadJson);
+        var a = listeTextbausteine.GetTextbaustein(id);
+
+        var html = alleTextbausteine.GetEinHtmlTextbaustein(a);
+
+        Assert.Equal(laenge, html.Length);
+    }
+}
